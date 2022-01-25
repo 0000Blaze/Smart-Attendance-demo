a71a297e314db8370e336cab3205f12a1490d3cd
0000BLAZE <rchhetry5@gmail.com>
2022-01-25 10:42:26 +0545

    initial commit

---
+++ data/getStudentImages.py
@@ -0,0 +1,41 @@
+import wget
+
+
+def imageDownloader(fixed_url,varying_url):
+    try:
+        url = fixed_url + varying_url + ".png"
+        file_name = wget.download(url)
+    except:
+        try:
+            url = fixed_url + varying_url + ".jpg"
+            file_name = wget.download(url)
+        except:
+            url = fixed_url + varying_url + ".jpeg"
+            file_name = wget.download(url)
+    print('Image Successfully Downloaded: ', file_name)
+
+#fixed url part for all PUL075BCT students
+fixed_url = "https://exam.ioe.edu.np/Images/StudentCurrentImage/3036/PUL075BCT"
+
+#Total number of students in batch
+i=96        
+while i>0:
+    if i<10:
+        varying_url = "00" + str(i)
+    elif i == 87 or i == 73  or i == 36 or i == 34 or i == 17:   #dropouts 87 73 36 17
+        i= i-1
+        continue
+    else:
+        varying_url = "0" + str(i)
+
+    imageDownloader(fixed_url,varying_url)
+    i = i-1
+
+#download images of new added students
+varying_url = "100"
+imageDownloader(fixed_url,varying_url)
+x= int(varying_url) - 1
+while x > 96:    
+    varying_url = "0" + str(x)
+    imageDownloader(fixed_url,varying_url)
+    x = x-1
---
+++ scraper/getStudentList.py
@@ -0,0 +1,58 @@
+from pydoc import source_synopsis
+from markupsafe import string
+import requests
+from requests import get
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+
+url ="http://doece.pcampus.edu.np/index.php/students-bachelor-in-computer-engineering/?fbclid=IwAR0WHHYvNonw4CVQXrXwzVnfx9gNQwkU86z84ncTLgkFx54Ue07cofXOep0"
+headers = {"Accept-Language":"en-US,en;q=0.5"}
+results = requests.get(url,headers=headers)
+
+soup = BeautifulSoup(results.text,"html.parser")
+
+#initiliate data storage
+rollNo =[]
+name =[]
+temp=[]
+
+#find the specific batch roll number and name
+matched_tags = soup.find_all(lambda tag:len(tag.find_all()) == 0 and "PUL075" in tag.text)      #change year as needed batch
+
+############################extracting the name and roll number into sparate lists###############################
+
+#converting bs4.element.tag into string list
+for matched_tag in matched_tags:
+    temp.append(str(matched_tag))
+
+#temporary lists for removing the <p> and </p> tags
+z1=[]
+z2=[]
+
+#split the roll number and name
+for x in temp:
+    y = x.split(" ",1) 
+    z1.append(y[0])
+    z2.append(y[1])
+
+#Get name list
+for x in z2:
+    x1= x.split("<",1)
+    name.append(x1[0])
+
+#Get roll number list
+for x in z1:
+    x1= x.split(">",1)
+    rollNo.append(x1[1])
+
+#################################### END extracting name and roll number #####################################
+
+#pandas dataframe
+records = pd.DataFrame({
+    'RollNo': rollNo,
+    'Name': name,
+})
+
+#convert pandas dataframe to csv file
+records.to_csv('records.csv')
