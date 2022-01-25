f1fb03a79bb018b5140113818298f4240a0d54e0
0000Blaze <rchhetry5@gmail.com>
2022-01-25 16:17:03 +0545

    made function to add new students to AB and CD records

--- scraper/recordHandler.py
+++ scraper/recordHandler.py
@@ -2,23 +2,58 @@
 #1. split classes AB and CD studnets
 #2. add the new transfer students
 
+from itertools import count
 from operator import index
 from statistics import variance
 import pandas as pd
+from csv import writer
 
 def splitClasses():
     chuck_size = 48
     batch_no = 1
+    text = "AB"
 
     for chunk in pd.read_csv('records.csv',chunksize=chuck_size):
-        chunk.to_csv('record'+ str(batch_no) + '.csv',index=False)
+        if batch_no == 1:
+            chunk.to_csv('record'+ text + '.csv',index=False)
+        else:
+            text = "CD"
+            chunk.to_csv('record'+ text + '.csv',index=False)
         batch_no = batch_no + 1
+    print("\nSuccesful split into two records\n")
+
+def addNewStudents():
+    #added required students record
+    dataAB=[
+        [96,'PUL075BCT097 ','BIBEK BASHYAL'],
+        [98,'PUL075BCT098 ','SAUGAT KAFLE']
+    ]
+
+    dataCD =[
+        [97,'PUL075BCT098 ','ACHYUT BURLAKOTI'],
+        [99,'PUL075BCT100 ','SIJAL BARAL']
+    ]
+
+    # Open our existing CSV file for AB class
+    with open('recordAB.csv', 'a',newline='') as f_object:
+        writer_object = writer(f_object)
+        writer_object.writerows(dataAB)
+        f_object.close()
+
+    # Open our existing CSV file for CD class
+    with open('recordCD.csv', 'a',newline='') as f_object:
+        writer_object = writer(f_object)
+        writer_object.writerows(dataCD)
+        f_object.close()
+    
+    print("\nSuccesful added new students in AB and CD\n")
+
 
 variable = input("Choose:\n 1.Split classes\n 2.Added new students\n")
 variable_int = int(variable)
 if variable_int == 1:
     splitClasses()
 elif variable_int == 2:
-    pass
+    addNewStudents()
 else:
     print("\n SOME ERROR OCCURED\n")
