2f48e8a527298d32b279145a7ca8dea2a7cb2a0a
0000Blaze <rchhetry5@gmail.com>
2022-01-31 21:27:25 +0545

    cleaning unnecessary lines

--- teacherSide/main.py
+++ teacherSide/main.py
@@ -1,34 +1,7 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
-
-#from kivy.uix.boxlayout import BoxLayout
-#from kivy.uix.image import Image
-#from kivy.uix.button import Button
-#from kivy.uix.label import Label
-
-
-#class TeacherApp(App):
-#    def build(self):
-#    
-#        self.img1 = Image(source="assets/logo.png",size_hint=(1, .8))
-#        self.verificationLabel = Label(
-#            text="Your solution to attendance", size_hint=(1, .1))
-#        self.btn1 = Button(text='Start Atteandance', size_hint=(1, .1))
-
-    	# layout of app
-#        layout = BoxLayout(orientation='vertical')
-#        layout.add_widget(self.img1)
-#        layout.add_widget(self.verificationLabel)
-#        layout.add_widget(self.btn1)
-#        
-#        return layout
-#
-#TeacherApp().run()
-
-from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+from kivy.uix.widget import Widget
 
 class MainWindow(Screen):
     pass
