5294810e5cb637ccbda6ca4ae5fecdbbc168632c
Ravi <075bct065.ravi@pcampus.edu.np>
2022-02-02 10:58:13 +0545

    UI made respective to screen size.

--- teacherSide/main.py
+++ teacherSide/main.py
@@ -1,9 +1,13 @@
+import imp
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.properties import NumericProperty
+# Window.size = (540, 960)
 
 
 class MainWindow(Screen):
@@ -13,14 +17,18 @@ class MainWindow(Screen):
 class BctWindow(Screen):
     pass
 
+
 class BexWindow(Screen):
     pass
+
 
 class BelWindow(Screen):
     pass
 
+
 class SectionWindow(Screen):
     pass
+
 
 class BctCDWindow(Screen):
     pass
@@ -34,7 +42,11 @@ kv = Builder.load_file("my.kv")
 
 
 class TeacherApp(App):
+    winH = NumericProperty(Window.size[0])
+    winW = NumericProperty(Window.size[1])
+
     def build(self):
+        print(type(self.winH))
         return kv
 
 
