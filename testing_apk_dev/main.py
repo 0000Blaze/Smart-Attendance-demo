0d26b04be059ee753654f94509dc8e586f49d922
0000Blaze <rchhetry5@gmail.com>
2022-02-06 10:56:21 +0545

    testing out image capture with kivy_garden

--- testing_apk_dev/main.py
+++ testing_apk_dev/main.py
@@ -1,27 +1,59 @@
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.textinput import TextInput
+from kivy.properties import NumericProperty, ObjectProperty, StringProperty
+# Window.size = (540, 960)
 
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.image import Image
-from kivy.uix.button import Button
-from kivy.uix.label import Label
+
+class RollNoInput(Widget):
+    field_id = ObjectProperty(None)
+    field_text = StringProperty(None)
+    field_placeholder = StringProperty(None)
+    text = ""
+
+    def getText(self):
+        return self.field_text
+
+    def getPlaceHolder(self):
+        return self.field_placeholder
+
+    def process(self, txt):
+        if txt.text != "":
+            self.text = txt.text
+            print(self.text)
+        else:
+            print("text empty")
+    pass
 
 
-class TeacherApp(App):
+class MainWindow(Screen):
+    stdRoll = RollNoInput()
+    stdRoll.field_id = ObjectProperty(None)
+    stdRoll.field_text = 'Roll No:'
+    stdRoll.field_placeholder = 'Enter Roll no:'
+    stdRoll.text = ""
+    pass
+
+
+class CameraWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file("style.kv")
+
+
+class StudentApp(App):
+
     def build(self):
-    
-        self.img1 = Image(source="assets/logo.png",size_hint=(1, .8))
-        self.verificationLabel = Label(
-            text="Your solution to attendance", size_hint=(1, .1))
-        self.btn1 = Button(text='Start Atteandance', size_hint=(1, .1))
+        return kv
 
-    	# layout of app
-        layout = BoxLayout(orientation='vertical')
-        layout.add_widget(self.img1)
-        layout.add_widget(self.verificationLabel)
-        layout.add_widget(self.btn1)
-        
-        return layout
 
-TeacherApp().run()
-
+if __name__ == "__main__":
+    StudentApp().run()
