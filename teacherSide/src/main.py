0d6832b2160130f5a74a1175234a4ac14c107319
0000Blaze <rchhetry5@gmail.com>
2022-02-18 16:52:26 +0545

    trying to make dynamic attendance code label

--- teacherSide/src/main.py
+++ teacherSide/src/main.py
@@ -9,6 +9,7 @@ import imp
 from kivy.app import App
 from kivy.lang import Builder
 
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
@@ -21,19 +22,14 @@ from kivy.core.window import Window
 from kivy.properties import NumericProperty, StringProperty,ObjectProperty
 from kivy.utils import platform
 from kivy.clock import mainthread
-from kivy.logger import Logger
 
+''' classes for various data to and fro from kv and python side '''
+#teacher id input class
 class TeacherIdInput(Widget):
     field_id = ObjectProperty(None)
     field_text = StringProperty(None)
     field_placeholder = StringProperty(None)
-
-    def getText(self):
-        return self.field_text
-
-    def getPlaceHolder(self):
-        return self.field_placeholder
-
+    
     def setTeacherId(self, app, textIp):
         if textIp != "":
             app.teacherId = textIp.text
@@ -41,7 +37,6 @@ class TeacherIdInput(Widget):
         else:
             print("text empty")
     pass
-
     def setClassId(self, app, textIp):
         if textIp != "":
             app.classId = textIp.text.upper()
@@ -50,16 +45,10 @@ class TeacherIdInput(Widget):
             print("text empty")
     pass
 
+#subject id selection class
 class SubjectSelect(Widget):
     field_id = ObjectProperty(None)
     field_text = StringProperty(None)
-    field_placeholder = StringProperty(None)
-
-    def getText(self):
-        return self.field_text
-
-    def getPlaceHolder(self):
-        return self.field_placeholder
 
     def setSubjectCode(self, app, textIp):
         if textIp != "":
@@ -69,17 +58,25 @@ class SubjectSelect(Widget):
             print("text empty")
     pass
 
+#attendance code class
+class AttendanceCode(Widget):
+    field_id = ObjectProperty(None)
+    field_text = StringProperty(None)
+    pass
+'''      Classes for kivy windows   '''
+class AttendanceLabel(Label):
+    pass
 
 class MainWindow(Screen):
     stdTid = TeacherIdInput()
     stdTid.field_id = ObjectProperty(None)
     stdTid.field_text = 'Teacher Id:'
-    stdTid.field_placeholder = 'Enter Teacher Id:'
-    
+    stdTid.field_placeholder = 'Example : 011'
+
     stdClass = TeacherIdInput()
     stdClass.field_id = ObjectProperty(None)
     stdClass.field_text = 'Class Id:'
-    stdClass.field_placeholder = 'Enter Class Id:'
+    stdClass.field_placeholder = 'Example : 075BCTCD'
 
     prevColor = [1, 1, 1, 1]
     def buttonPressed(self, btn, recentUsedColor):
@@ -95,20 +92,24 @@ class MainWindow(Screen):
 
 
 class SubjectSelectWindow(Screen):
-    
     stdSid = SubjectSelect()
     stdSid.field_id = ObjectProperty(None)
     stdSid.field_text = 'Subject Id:'
-    stdSid.field_placeholder = ''
-
     pass
 
 class AttendanceControlWindow(Screen):
-    pass
+    attendanceCode = AttendanceCode()
+    attendanceCode.field_id = ObjectProperty(None)        
+    attendanceCode.field_text = 'No attendance code'
+    def setAttendanceCode(self):
+        try:
+            self.ids.attendance_code.text = TeacherApp.attendanceId
+            print("hi",self.ids.attendance_code.text,TeacherApp.attendanceId)
+        except:
+            print("error")    
 
 class WindowManager(ScreenManager):
     pass
-
 
 kv = Builder.load_file("my.kv")
 
@@ -117,6 +118,8 @@ class TeacherApp(App):
     teacherId =""
     classId = ""
     subjectId = ""
+    attendanceId =""
+
     
     def build(self):
         # print(type(self.winH))
@@ -125,14 +128,23 @@ class TeacherApp(App):
     def startAttendanceSheet(self):
         try:
             AttendanceListFromServer = client_teacher.startAttendance(self.teacherId, self.classId, self.subjectId)
-            #print("started attendance with no error")
-            #print(AttendanceListFromServer)
             if "error" in AttendanceListFromServer:
                 print(AttendanceListFromServer["error"])
             else:
-                print(AttendanceListFromServer["acode"])
-                print(AttendanceListFromServer["student_list"])
+                #save attendance code
+                TeacherApp.attendanceId = AttendanceListFromServer["acode"]
+                print(TeacherApp.attendanceId)
+                #save and display records
+                #layout = self.ids['attendanceList']
+                for list in AttendanceListFromServer["student_list"]:
+                    print(list[0], list[1])
+                    #lab1 = AttendanceLabel(text=str(list[0]),size_hint_x=.35, halign='left' )
+                    #lab2 = AttendanceLabel(text=str(list[1]),size_hint_x=.15, halign='right' )
+                    #layout.add_widget(lab1)
+                    #layout.add_widget(lab2)
+                #print(type(AttendanceListFromServer["student_list"]))
                 print(AttendanceListFromServer["timeout"])
+                print(TeacherApp.attendanceId)
         except Exception as e:
             print("error :", e)
 
@@ -140,8 +152,6 @@ class TeacherApp(App):
     def updateAttendanceSheet(self):
         try:
             AttendanceListFromServer = client_teacher.getAttendance(self.teacherId,self.classId)
-            #print("Refresh update list")
-            #print(AttendanceListFromServer)
             if "error" in AttendanceListFromServer:
                 print(AttendanceListFromServer["error"])
             else:
@@ -152,8 +162,6 @@ class TeacherApp(App):
     def finalAttendanceSheet(self):
         try:
             AttendanceListFromServer = client_teacher.stopAttendance(self.teacherId,self.classId)
-            #print("Stoped attendance and got response")
-            #print(AttendanceListFromServer)
             if "error" in AttendanceListFromServer:
                 print(AttendanceListFromServer["error"])
             else:
