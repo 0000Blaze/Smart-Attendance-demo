9e8e2c16e0a97c4c6a7b8817de4427bb59b0c3f9
0000Blaze <rchhetry5@gmail.com>
2022-02-25 15:45:44 +0545

    webApp css styling done

---
+++ webApp/php/connectToDb.php
@@ -0,0 +1,7 @@
+<?php
+$sqldb = new mysqli('localhost', 'root', 'Ashutosh123$');
+if (!$sqldb) {
+    die('Couldn\'t connect to the sql server');
+} else {
+    //  echo "<p>Connected to sql server";
+}
